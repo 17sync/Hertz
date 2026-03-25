1b4ed501a6f50ba7aab5bf3be13ff3cbd17da04c
r1v win10 <actuallyaun@gmail.com>
2026-03-25 20:10:42 +0500

    Modified songs list to make all newly added files in the songs folder

--- src/script.js
+++ src/script.js
@@ -1,6 +1,30 @@
 const songs=[
-  {title:"Song 1", src:"songs/song1.mp3"},
-  {title:"Song 2", src:"songs/song2.mp3"}
+  {title:"Joji - 777", src:"../songs/Joji - 777.mp3"},
+  {title:"Joji - COME THRU", src:"../songs/Joji - COME THRU.mp3"},
+  {title:"Joji - Ew", src:"../songs/Joji - Ew.mp3"},
+  {title:"Joji - Gimme Love", src:"../songs/Joji - Gimme Love.mp3"},
+  {title:"Joji - High Hopes", src:"../songs/Joji - High Hopes.mp3"},
+  {title:"Joji - I'LL SEE YOU IN 40", src:"../songs/Joji - I'LL SEE YOU IN 40.mp3"},
+  {title:"Joji - Like You Do", src:"../songs/Joji - Like You Do.mp3"},
+  {title:"Joji - MODUS", src:"../songs/Joji - MODUS.mp3"},
+  {title:"Joji - Mr. Hollywood", src:"../songs/Joji - Mr. Hollywood.mp3"},
+  {title:"Joji - NO NITROUS", src:"../songs/Joji - NO NITROUS.mp3"},
+  {title:"Joji - Normal People (ft. rei brown)", src:"../songs/Joji - Normal People (ft. rei brown).mp3"},
+  {title:"Joji - Pretty Boy (ft. Lil Yachty)", src:"../songs/Joji - Pretty Boy (ft. Lil Yachty).mp3"},
+  {title:"Joji - Reanimator (ft. Yves Tumor)", src:"../songs/Joji - Reanimator (ft. Yves Tumor).mp3"},
+  {title:"Joji - Run", src:"../songs/Joji - Run.mp3"},
+  {title:"Joji - Sanctuary", src:"../songs/Joji - Sanctuary.mp3"},
+  {title:"Joji - SLOW DANCING IN THE DARK", src:"../songs/Joji - SLOW DANCING IN THE DARK.mp3"},
+  {title:"Joji - TEST DRIVE", src:"../songs/Joji - TEST DRIVE.mp3"},
+  {title:"Joji - Tick Tock", src:"../songs/Joji - Tick Tock.mp3"},
+  {title:"Joji - WANTED U", src:"../songs/Joji - WANTED U.mp3"},
+  {title:"Joji - WHY AM I STILL IN LA (ft. Shlohmo & D33J)", src:"../songs/Joji - WHY AM I STILL IN LA (ft. Shlohmo & D33J).mp3"},
+  {title:"Joji - YEAH RIGHT", src:"../songs/Joji - YEAH RIGHT.mp3"},
+  {title:"Joji - Your Man", src:"../songs/Joji - Your Man.mp3"},
+  {title:"Joji & Diplo - Daylight", src:"../songs/Joji & Diplo - Daylight.mp3"},
+  {title:"Joji & BENEE - Afterthought", src:"../songs/Joji & BENEE - Afterthought.mp3"},
+  {title:"Joji ft. Clams Casino - CAN'T GET OVER YOU", src:"../songs/Joji ft. Clams Casino - CAN'T GET OVER YOU.mp3"},
+  {title:"Joji ft. Trippie Redd - R.I.P", src:"../songs/Joji ft. Trippie Redd - R.I.P.mp3"}
 ];
 let currentIndex=0;
 
