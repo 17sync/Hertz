3f4891293ea1c8e8ed5ccbc87037585c705e024c
r1v win10 <actuallyaun@gmail.com>
2026-03-26 18:34:23 +0500

    Changed songs array to paths only, Added album covers functionality

--- src/script.js
+++ src/script.js
@@ -1,31 +1,36 @@
 const songs=[
-  {title:"Joji - 777", src:"../songs/Joji - 777.mp3"},
-  {title:"Joji - COME THRU", src:"../songs/Joji - COME THRU.mp3"},
-  {title:"Joji - Ew", src:"../songs/Joji - Ew.mp3"},
-  {title:"Joji - Gimme Love", src:"../songs/Joji - Gimme Love.mp3"},
-  {title:"Joji - High Hopes", src:"../songs/Joji - High Hopes.mp3"},
-  {title:"Joji - I'LL SEE YOU IN 40", src:"../songs/Joji - I'LL SEE YOU IN 40.mp3"},
-  {title:"Joji - Like You Do", src:"../songs/Joji - Like You Do.mp3"},
-  {title:"Joji - MODUS", src:"../songs/Joji - MODUS.mp3"},
-  {title:"Joji - Mr. Hollywood", src:"../songs/Joji - Mr. Hollywood.mp3"},
-  {title:"Joji - NO NITROUS", src:"../songs/Joji - NO NITROUS.mp3"},
-  {title:"Joji - Normal People (ft. rei brown)", src:"../songs/Joji - Normal People (ft. rei brown).mp3"},
-  {title:"Joji - Pretty Boy (ft. Lil Yachty)", src:"../songs/Joji - Pretty Boy (ft. Lil Yachty).mp3"},
-  {title:"Joji - Reanimator (ft. Yves Tumor)", src:"../songs/Joji - Reanimator (ft. Yves Tumor).mp3"},
-  {title:"Joji - Run", src:"../songs/Joji - Run.mp3"},
-  {title:"Joji - Sanctuary", src:"../songs/Joji - Sanctuary.mp3"},
-  {title:"Joji - SLOW DANCING IN THE DARK", src:"../songs/Joji - SLOW DANCING IN THE DARK.mp3"},
-  {title:"Joji - TEST DRIVE", src:"../songs/Joji - TEST DRIVE.mp3"},
-  {title:"Joji - Tick Tock", src:"../songs/Joji - Tick Tock.mp3"},
-  {title:"Joji - WANTED U", src:"../songs/Joji - WANTED U.mp3"},
-  {title:"Joji - WHY AM I STILL IN LA (ft. Shlohmo & D33J)", src:"../songs/Joji - WHY AM I STILL IN LA (ft. Shlohmo & D33J).mp3"},
-  {title:"Joji - YEAH RIGHT", src:"../songs/Joji - YEAH RIGHT.mp3"},
-  {title:"Joji - Your Man", src:"../songs/Joji - Your Man.mp3"},
-  {title:"Joji & Diplo - Daylight", src:"../songs/Joji & Diplo - Daylight.mp3"},
-  {title:"Joji & BENEE - Afterthought", src:"../songs/Joji & BENEE - Afterthought.mp3"},
-  {title:"Joji ft. Clams Casino - CAN'T GET OVER YOU", src:"../songs/Joji ft. Clams Casino - CAN'T GET OVER YOU.mp3"},
-  {title:"Joji ft. Trippie Redd - R.I.P", src:"../songs/Joji ft. Trippie Redd - R.I.P.mp3"}
+  "songs/nectar/Joji - 777.mp3",
+  "songs/ballads1/Joji - COME THRU.mp3",
+  "songs/nectar/Joji - Ew.mp3",
+  "songs/nectar/Joji - Gimme Love.mp3",
+  "songs/nectar/Joji - High Hopes.mp3",
+  "songs/ballads1/Joji - I'LL SEE YOU IN 40.mp3",
+  "songs/nectar/Joji - Like You Do.mp3",
+  "songs/nectar/Joji - MODUS.mp3",
+  "songs/nectar/Joji - Mr. Hollywood.mp3",
+  "songs/nectar/Joji - NITROUS.mp3",
+  "songs/nectar/Joji - Normal People (ft. rei brown).mp3",
+  "songs/nectar/Joji - Pretty Boy (ft. Lil Yachty).mp3",
+  "songs/nectar/Joji - Reanimator (ft. Yves Tumor).mp3",
+  "songs/nectar/Joji - Run.mp3",
+  "songs/nectar/Joji - Sanctuary.mp3",
+  "songs/ballads1/Joji - SLOW DANCING IN THE DARK.mp3",
+  "songs/ballads1/Joji - TEST DRIVE.mp3",
+  "songs/nectar/Joji - Tick Tock.mp3",
+  "songs/ballads1/Joji - WANTED U.mp3",
+  "songs/ballads1/Joji - WHY AM I STILL IN LA (ft. Shlohmo & D33J).mp3",
+  "songs/ballads1/Joji - YEAH RIGHT.mp3",
+  "songs/nectar/Joji - Your Man.mp3",
+  "songs/nectar/Joji & Diplo - Daylight.mp3",
+  "songs/nectar/Joji & BENEE - Afterthought.mp3",
+  "songs/ballads1/Joji ft. Clams Casino - CAN'T GET OVER YOU.mp3",
+  "songs/ballads1/Joji ft. Trippie Redd - R.I.P.mp3"
 ];
+const albumCovers={
+  "nectar":"../album-covers/nectar.png",
+  "ballads1":"../album-covers/ballads1.png"
+};
+
 let currentIndex=0;
 
 const audio=document.getElementById("audio");
@@ -33,11 +38,32 @@ const playButton=document.getElementById("play");
 const nextButton=document.getElementById("next");
 const prevButton=document.getElementById("prev");
 const title=document.getElementById("title");
+const cover=document.getElementById("cover");
+
+function getAlbum(song){
+  return song.split("/")[1]; 
+}
+
+function getCover(song){
+  const album=getAlbum(song);
+  
+  if(albumCovers[album]) return albumCovers[album];
+  return "../album-covers/defaultCover.png";
+}
+
+function getTitle(song){
+  let name=song.split("/").pop();        
+  name=name.replace(".mp3","");            
+  
+  return name;
+}
 
 function loadSong(index){
   const song=songs[index];
-  audio.src=song.src;
-  title.innerText=song.title;
+  
+  cover.src=getCover(song);
+  audio.src=song;
+  title.innerText=getTitle(song);
 }
 
 let isPlaying=false;
